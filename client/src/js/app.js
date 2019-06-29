4dfca607fd1e1b3477ff10d328d3d32a3bc587f1
Leonardo Santos <sds.leo2@gmail.com>
2019-06-29 15:47:05 -0300

    nova estrutura
    ...

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,71 +1,33 @@
-import $$ from 'dom7';
-import Framework7 from 'framework7/framework7.esm.bundle.js';
+moment.locale('pt-BR');
+const api = 'http://loreweb.com.br/janbook/api';
 
-// Import F7 Styles
-import 'framework7/css/framework7.bundle.css';
+// Dom7
+const $$ = Dom7;
 
-// Import Icons and App Custom Styles
-import '../css/icons.css';
-import '../css/app.scss';
-
-// Import Routes
-import routes from './routes.js';
-
+// Init App
 var app = new Framework7({
-  root: '#app', // App root element
-
-  name: 'hackathon', // App name
-  theme: 'auto', // Automatic theme detection
-  // App root data
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-      // Demo products for Catalog section
-      products: [
-        {
-          id: '1',
-          title: 'Apple iPhone 8',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-        },
-        {
-          id: '2',
-          title: 'Apple iPhone 8 Plus',
-          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-        },
-        {
-          id: '3',
-          title: 'Apple iPhone X',
-          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-        },
-      ]
-    };
-  },
-  // App root methods
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
-    },
-  },
-  // App routes
-  routes: routes,
-
-  // Register service worker
-  serviceWorker: {
-    path: '/service-worker.js',
-  },
+  name: 'JANBOOK',
+  id: 'br.com.janbook',
+  root: '#app',
+  theme: 'auto',
+  routes: routes
 });
 
-// Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
 
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
+document.addEventListener("deviceready", function(){
+  document.addEventListener('backbutton', f.onBackKeyDown, false);
+}, false);
 
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+
+$$('#logoIntro').animationEnd(function(){  
+  setTimeout(function(){
+    app.router.navigate('/login/');
+  }, 2000);
 });
+
+
+
+
+
+
+
