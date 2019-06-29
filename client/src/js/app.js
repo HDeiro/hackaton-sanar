8318a1096374de950016276aba9092da314a0b96
Leonardo Santos <sds.leo2@gmail.com>
2019-06-29 17:43:28 -0300

    arquivos adicionados assets

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -21,12 +21,27 @@ document.addEventListener("deviceready", function(){
 
 $$('#logoIntro').animationEnd(function(){  
   setTimeout(function(){
-    app.router.navigate('/login/');
+    // app.router.navigate('/login/');
+      authInfo.open();
   }, 2000);
 });
 
 
-
+var authInfo = app.dialog.create({
+  title: 'Olá Gamer, eu sou o Léo',
+  text: `
+    <p>
+      Vejo que você não tem registro do COMBATE J neste dispositivo.
+    </p>
+    <p>
+      Você tem duas opções.
+    </p>
+    <div class="row" style="padding-top: 20px;">
+      <button onclick="game.modalAuth(2);" class="col button button-fill">Cadastre-se</button>
+      <button onclick="game.modalAuth(3);" class="col button button-fill">Fazer Login</button>
+    </div>
+  `
+});
 
 
 
